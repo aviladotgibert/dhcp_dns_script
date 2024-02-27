72b3b7f374e2e1fa3e64360b54e0033628a2fb88
Alex Viladot Gibert <63657502+aviladotgibert@users.noreply.github.com>
2024-02-27 20:48:05 +0100

    Update dhcpdns_script.sh

--- dhcpdns_script.sh
+++ dhcpdns_script.sh
@@ -10,13 +10,20 @@ apt install -y bind9 isc-dhcp-server git
 git clone https://github.com/aviladotgibert/dhcp_dns_script.git
 
 # Copiar la configuración de dhcp y bind
-cp -r dhcp_dns_script/dhcp /etc/
-cp -r dhcp_dns_script/bind /etc/
+cp -r -f dhcp_dns_script/dhcp /etc/dhcp/
+cp -r -f dhcp_dns_script/bind /etc/bind/
 
 # Copiar otros archivos de configuración
-cp -r dhcp_dns_script/etc/* /etc/
+cp -r -f dhcp_dns_script/etc/* /etc/
+
+#Arreglar lo que dijo Alina del enlace simbolico para el resolv.conf
+sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # Cambiar el propietario y grupo de los archivos en /etc/bind a bind:bind
 chown -R bind:bind /etc/bind
+
+#Reiniciamos servicios para guardar cambios
+systemctl restart isc-dhcp-server
+systemctl restart bind9
 
 echo "Configuración completada."
